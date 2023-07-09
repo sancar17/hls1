44ab83a002e5f23f77648cb107c3534d93b94fd7
SathvikSetty <43770983+SathvikSat@users.noreply.github.com>
2023-07-09 14:38:48 +0200

    Update conv.cpp With sample test case and suitable pragma factor loop carry dep is resolved

--- conv.cpp
+++ conv.cpp
@@ -4,25 +4,42 @@ float* conv2d_hls( TwoDTile_IPT x[],  TwoD_filterT Kernel_i[], float z[],  float
 {
 	//float z_bigTile_per_Opchnl[outDim] ={0};
 	unsigned short int numTilesCols = inputWidth - KernelSize +1;
-
+	//unsigned short int shiftAcc = 0;
 	for (unsigned short int tileCol = 0; tileCol < numTilesCols; tileCol++)
 	{
+		float acc[KernelSize*KernelSize*num_chnl_ip] = {0};
 		for (size_t c = 0; c < num_chnl_ip; c++)
 		{
+#pragma HLS PIPELINE II=100
 			for (size_t p = 0; p < KernelSize; p++)
     		{
+
         		for (size_t q = tileCol; q < (tileCol+KernelSize); q++)
         		{
-					z[tileCol]  += x[c].tileData[p][q] * Kernel_i[c].W_c[p][q-tileCol];
-				}
+        			acc[(q-tileCol) + ( (KernelSize*p) + (c*KernelSize*KernelSize) ) ] = x[c].tileData[p][q] * Kernel_i[c].W_c[p][q-tileCol];
+					//z[tileCol]  += x[c].tileData[p][q] * Kernel_i[c].W_c[p][q-tileCol];
+        			//z[tileCol] += acc[q-tileCol];
+        		}
+        		//shiftAcc = kernelSize;
+
 			}
+
 		}
+		for(size_t m = 0; m < KernelSize*KernelSize*num_chnl_ip; m++)
+		{
+			#pragma HLS UNROLL  factor=3455
+			#pragma HLS PIPELINE II=1
+			z[tileCol] += acc[m];
+		}
+
+
+
 	}
 
 	for (size_t j = 0; j < outDim; j++)
 	{
 		z[j] += b;
-		//printf("z_bigTile_per_Opchnl: %f\n", z_bigTile_per_Opchnl[j]);
+		printf("z: %f\n", z[j]);
 	}
 	return z;
 
@@ -43,17 +60,17 @@ void EntryConv(TwoD_IPT X[num_chnl_ip],  TwoD_wtT W[num_chnl_op], TwoD_outT Z[nu
 
     size_t rowSize = sizeof(float) * inputWidth * KernelSize;
 
-#pragma HLS UNROLL factor=inputWidth -KernelSize +1
+//#pragma HLS UNROLL factor=inputWidth -KernelSize +1
 	for (size_t readImgRow = 0; readImgRow < (inputWidth -KernelSize +1); readImgRow++)
 	{
-#pragma HLS UNROLL factor=num_chnl_ip
+//#pragma HLS UNROLL factor=num_chnl_ip
 
 		//printf("readImgRow %d", readImgRow);
 		for (size_t i = 0; i < num_chnl_ip; i++)
 		{
-#pragma HLS UNROLL factor=KernelSize
+//#pragma HLS UNROLL factor=KernelSize
 			for(size_t row=0; row < KernelSize; row++){
-#pragma HLS UNROLL factor=inputWidth
+//#pragma HLS UNROLL factor=inputWidth
 				for(size_t col=0; col<inputWidth; col++){
 					x_row[i].tileData[row][col] = X[i].X_c[row+readImgRow][col];
 				}
@@ -63,14 +80,14 @@ void EntryConv(TwoD_IPT X[num_chnl_ip],  TwoD_wtT W[num_chnl_op], TwoD_outT Z[nu
 		}
 		for (size_t i = 0; i < num_chnl_op; i++)
 		{
-#pragma HLS UNROLL factor=num_chnl_ip
+//#pragma HLS UNROLL factor=4
 			for (size_t j = 0; j < num_chnl_ip; j++)
 			{
-#pragma HLS UNROLL factor=KernelSize
+//#pragma HLS UNROLL factor=128
 				for(size_t row=0; row<KernelSize; row++){
-#pragma HLS UNROLL factor=KernelSize
+//#pragma HLS UNROLL factor=2
 					for(size_t col=0; col<KernelSize; col++){
-
+//#pragma HLS UNROLL factor=2
 						w_set[0].ith_filter[j].W_c[row][col] = W[i].ith_filter[j].W_c[row][col];
 					}
 				}
