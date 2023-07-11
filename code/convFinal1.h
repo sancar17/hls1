51430822891e9adc2e35b973301e6992da3feaf1
SathvikSetty <43770983+SathvikSat@users.noreply.github.com>
2023-07-11 15:17:25 +0200

    Create convFinal1.h
    associated with convFinal1.cpp 15:15 commit.

---
+++ code/convFinal1.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <cstring>
+#define chnl_rdn_factor 1
+#define inputWidth  16//6//33//6
+#define KernelSize  3//4//3//4
+#define num_chnl_op 384//3//256//3
+#define num_chnl_ip 384//2//256//2
+#define NumKernels  384//3//256//3
+#define outDim (inputWidth-KernelSize+1)
+
+
+typedef struct TwoD_struct_input {
+    float X_c[inputWidth][inputWidth];
+} TwoD_IPT;
+
+typedef struct TwoD_struct_kernel {
+    float W_c[KernelSize][KernelSize];
+} TwoD_filterT;
+
+typedef struct TwoD_struct_wt_nonDRAM {
+
+    TwoD_filterT ith_filter[num_chnl_ip/chnl_rdn_factor];
+
+} TwoD_nonDRAM_wtT;
+
+typedef struct TwoD_struct_wt {
+
+    TwoD_filterT ith_filter[num_chnl_ip];
+
+} TwoD_wtT;
+
+typedef struct TwoD_struct_out {
+    float Z_out[outDim][outDim];
+
+}TwoD_outT;
+
+typedef struct TwoDTile_IPT_tile{
+	float tileData[KernelSize][inputWidth];
+}TwoDTile_IPT;
+
+void EntryConv(TwoD_IPT X[num_chnl_ip],  TwoD_wtT W[num_chnl_op], TwoD_outT Z[num_chnl_op],  float b[num_chnl_op] );
